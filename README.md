b72eb8890ca03b4680126dd242d86b645db39cda
JRichardsz <jrichardsz.java@gmail.com>
2024-03-11 17:05:13 -0500

    version 1.0.0  (#11)
    Features:
    
    - #2 
    - #3 
    - #7

--- README.md
+++ README.md
@@ -1,1 +1,143 @@
-# sql_tracer
+# Sql Tracer
+
+<!-- <p float="left">
+  <img src="./.coverage/branches.svg">
+  <img src="./.coverage/functions.svg">
+  <img src="./.coverage/lines.svg">
+  <img src="./.coverage/statements.svg">
+</p> -->
+
+
+<p align="center">
+  <img src="./.assets/logo.png" width=200 ></img>
+</p>
+
+A small tool to know how fast your database responds.
+
+## Settings
+
+|name| sample value |description|
+|:--|:--|:--|
+|-a| seus_01 | an alias of the server|
+|-h| 10.10.20.30 | database host like localhost, 127.0.0.1, an ip, etc|
+|-p| 1521 | database port: 1521 for oracle, 3306 for mysql, etc|
+|-u| root | database connection user|
+|-n| employee_db | database name. For oracle set the service name|
+|-r| /tmp/database_connection_report.csv  | a location to the desired csv report|
+|-i| 15 | value in seconds to be used a interval of execution|
+
+The password is a special and sensible value, so we cannot set it directly. You will
+
+For linux/mac
+
+```
+export sql_tracer_database_password=admin123
+```
+
+And for windows you can use the [Environment Variable Manager](https://github.com/usil/sql_tracer/wiki/Environment-Variables-Manager)
+
+## Usage
+
+- Dowload the latest release from [here](https://github.com/usil/sql_tracer/releases)
+- Configure the password as was explained previously
+- Open a shell and run this
+
+
+**Linux/Mac**
+
+```
+./sql_tracer -a seus_01 -p 1521 -h 10.10.20.30  -u system  -n xe -r /tmp/database_connection_report.csv -i 5
+```
+
+**Windows**
+
+```
+sql_tracer.exe -a seus_01 -p 1521 -h 10.10.20.30  -u system  -n xe  -r C:\\Users\\Jane\\Desktop\\database_connection_report.csv -i 5
+```
+
+
+## Report
+
+- client ip
+- client alias
+- ping start datetime
+- ping end datetime
+- status (success or error)
+- elapsed time in millis
+- error summary (one line)
+
+All the metrics will be stored in the provided report location. Example:
+
+<img src="https://github.com/usil/sql_tracer/assets/3322836/5665ee58-cb9f-4057-9dd1-d2317743b647" width=600>
+
+## Requirements
+
+- Go (https://go.dev/doc/install)
+
+## Dependencies
+
+```
+go get github.com/sijms/go-ora/v2
+```
+
+## Run for developers
+
+Set the database password as was explained.
+
+Then execute for **Linux/Mac**
+
+```
+go run src/main/go/sql_tracer.go -a seus_01 -p 1521 -h 10.10.20.30  -u system  -n xe -r /tmp/database_connection_report.csv -i 5
+```
+
+And for windows
+
+```
+go run src/main/go/sql_tracer.go -a seus_01 -p 1521 -h 10.10.20.30  -u system  -n xe  -r C:\\Users\\Jane\\Desktop\\database_connection_report.csv -i 5
+```
+
+## Build
+
+**Linux/Mac**
+
+```
+go build -o build/sql_tracer src/main/go/sql_tracer.go
+```
+
+**Windows**
+
+```
+go build -o build/sql_tracer.exe src/main/go/sql_tracer.go
+```
+
+If no errors, you will find the executable in the build folder. Then you could use it in a machine without **go**
+
+## Error codes
+
+|code| description|
+|:--|:--|
+|6660| I was not able to enter to that catch :/ |
+|6661| usually bad host or credentials |
+|6662| Connected but query validation got an error |
+|66650| report exist but cannot be accesed to append it |
+
+## Advanced settings
+
+For notitifations, templates, plugins, etc go to the [wiki](https://github.com/usil/sql_tracer/wiki)
+
+## Acknowledgments
+
+- https://www.needpix.com/photo/102486/speedometer-tachometer-speed-indicator-speed-speedo-measure
+
+## Contributors
+
+<table>
+  <tbody>    
+    <td>
+      <img src="https://avatars0.githubusercontent.com/u/3322836?s=460&v=4" width="100px;"/>
+      <br />
+      <label><a href="http://jrichardsz.github.io/">JRichardsz</a></label>
+      <br />
+    </td>
+  </tbody>
+</table>
---
+++ src/main/go/sql_tracer.go
@@ -0,0 +1,129 @@
+package main
+import (
+    "fmt"
+    _ "github.com/sijms/go-ora/v2"
+    "database/sql"
+    "flag"
+    "strconv"
+    "os"
+    "time"
+    "strings"
+)
+func main() {
+
+    port := flag.Int("p", 1521, "port")
+    host := flag.String("h", "localhost", "host")
+    user := flag.String("u", "foo", "user")
+    //@TODO: password is moved temporary to env 
+    //password := flag.String("ps", "changeme", "password")
+    password := os.Getenv("sql_tracer_database_password")
+    databaseName := flag.String("n", "acme", "service name")
+    alias := flag.String("a", "acme_db", "a human alias to the host from where is running")
+    reportLocation := flag.String("r", "/tmp/report.csv", "a csv f-null location to append the metrics")
+    intervalInSeconds := flag.Int("i", 15, "interval of execution in seconds")
+
+    //@TODO: get ip
+    // localIp,localIpError  := getLocalIp()
+    // if localIpError == nil {
+    //     fmt.Println(localIpError)
+    // }
+    localIp := "192.168.0.1"
+
+    flag.Parse()
+    fmt.Println("ip", localIp)
+    fmt.Println("alias", *alias)
+    fmt.Println("host", *host)
+    fmt.Println("port", *port)
+    fmt.Println("user", *user)
+    fmt.Println("password", "****")
+    fmt.Println("database name", *databaseName)
+    fmt.Println("reportLocation", *reportLocation)
+
+    connectionStringForLog := fmt.Sprintf("oracle://%s:%s@%s:%s/%s", *user, "*****", *host, strconv.Itoa(*port), *databaseName)
+    fmt.Println("connection string", connectionStringForLog)
+
+    connectionString := fmt.Sprintf("oracle://%s:%s@%s:%s/%s", *user, password, *host,  strconv.Itoa(*port), *databaseName)   
+    
+    for range time.Tick(time.Second * time.Duration(*intervalInSeconds)) {
+        start := time.Now()
+
+        conn, errCon := sql.Open("oracle", connectionString)
+        if errCon != nil {
+            fmt.Println(errCon)
+            saveReport(localIp, *alias, start, time.Now(), "failed", "error code 6660: "+strings.ReplaceAll(errCon.Error(), ",", ";"), *reportLocation)
+            continue
+        }
+        errCon = conn.Ping()
+        if errCon != nil {
+            fmt.Println(errCon)
+            saveReport(localIp, *alias, start, time.Now(), "failed", "error code 6661: "+strings.ReplaceAll(errCon.Error(), ",", ";"), *reportLocation)
+            continue
+        }
+        fmt.Println("\nconnection status: success")
+        rows, errQuery := conn.Query("SELECT 1 from dual")
+        defer rows.Close()
+        if errQuery != nil {
+            fmt.Println(errQuery)
+            saveReport(localIp, *alias, start, time.Now(), "failed", "error code 6662: "+strings.ReplaceAll(errQuery.Error(), ",", ";"), *reportLocation)
+            continue
+        }
+        var (
+            id int64
+        )
+        for rows.Next() {
+            rows.Scan(&id)
+        }
+        fmt.Println("query validation result:", id)
+        saveReport(localIp, *alias, start, time.Now(), "success", "", *reportLocation)
+    }
+}
+
+//error string should not have the [,] char because [,] is the default column delimiter in csv
+func saveReport(ip string, alias string, start time.Time, end time.Time, status string, errAsString string, reportLocation string){
+
+    b, err := os.ReadFile(reportLocation)
+    if err != nil {
+        fmt.Println("report doesn't exist")
+        str := "client_ip, client_alias, start, end, status, elapsed_millis, error_trace\n"
+        //if not exist, initialize it with empty string
+        err = os.WriteFile(reportLocation, []byte(str), 0644)
+        if err != nil {
+            fmt.Println(err)
+        }
+    }
+    fmt.Println("previous file size in bytes:", len(b))
+
+    f, err := os.OpenFile(reportLocation, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+    if err != nil {
+        fmt.Println("report doesn't exist , error code 66650")
+        os.Exit(1)
+    }
+    
+    defer f.Close()
+    
+    //append new line
+    startMillis := start.UnixNano()/ int64(time.Millisecond)
+    endMillis := end.UnixNano()/ int64(time.Millisecond)
+    diff := endMillis - startMillis
+    newLine := fmt.Sprintf("%s, %s, %s, %s, %s, %s, %s\n",
+        ip, alias, start.Format(time.RFC3339), end.Format(time.RFC3339), status, strconv.Itoa(int(diff)), errAsString)
+    fmt.Println("new line", newLine)
+    if _, err = f.WriteString(newLine); err != nil {    
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
+
+// func getLocalIp(){
+    // conn, err := net.Dial("udp", "8.8.8.8:80")
+    // if err == nil {
+    //     fmt.Println("Failed to get the local ip")
+    //     localAddr := "unknown"
+    // }else{
+    //     defer conn.Close()
+    //     localAddr := conn.LocalAddr().(*net.UDPAddr)    
+    // }
+    // return localAddr    
+// }
+
+
